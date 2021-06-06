4b3eb72b5b8d197be56cf8990a0baf49f4e22756
Joy Ghosh <36255129+JoyGhoshs@users.noreply.github.com>
2021-06-06 01:02:04 -0400

    index

---
+++ be-a-fox/index.php
@@ -0,0 +1,7 @@
+<?php
+header('HTTP/1.1 1337 JohnyWakesTalks');
+header('Set-Cookie:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImNvZGUiOiJTVEFUX0NPREUifQ.Some_thing_is_missing_place_it.UQRIm1YkTqdjZM3_iX8-XVApPYtMZk4VdcIi4UV4Hx8');
+header("X-XSS-Protection: 1; mode=block");
+header('link: <sec.css>;rel=stylesheet');
+?>
+<!--Be Clever Like Fox !-->
